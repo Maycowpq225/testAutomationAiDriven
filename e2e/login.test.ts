747253076ddaad0bf2f81340ded871ab3755ee38
Maycowpq225 <maycowfellipo@hotmail.com>
2026-02-26 15:19:03 +0000

    agents and login feature

---
+++ e2e/login.test.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('Successfully logs in', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="username"]').press('CapsLock');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  
+  await expect(page.locator('.title')).toHaveText('Products');
+
+});
